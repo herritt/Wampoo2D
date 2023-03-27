4f607fc5e3fc46dd1ce099b9a15b54f26531ec57
Randy Herritt <randy.herritt@gmail.com>
2023-03-27 17:25:26 -0300

    Create card and deck class. Added cards to deck along with their textures

---
+++ Wampoo2DUnityProject/Assets/Scripts/Card.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int spaces = 0;
+    public bool isSpecialCard = false;
+    public bool isKillerCard = false;
+    public bool isStarterCard = false;
+    public bool isJoker = false;
+    public bool isFour = false;
+    public bool isJack = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
